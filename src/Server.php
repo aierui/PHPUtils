e12cea189f7344c5014d3b9d54e8707a9682ff36
石金融 <imland@outlook.com>
2018-02-23 11:14:15 +0800

    src/Server.php

--- src/Server.php
+++ src/Server.php
@@ -5,3 +5,19 @@
  * Date: 2018/2/15
  * Time: 下午11:11
  */
+
+namespace PHPUtils;
+
+
+class Server{
+
+    public static function getIp()
+    {
+        $ip = $_SERVER['SERVER_ADDR'];
+        if(empty($ip)) {
+            $ip = empty($_SERVER['SERVER_ADDR']) ? 0 : $_SERVER['SERVER_ADDR'];
+        }
+        return $ip;
+    }
+
+}
