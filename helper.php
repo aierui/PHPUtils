b2a3ec06cb1a90c9940cad2a894e868dab0c3649
石金融 <aieruishi@gmail.com>
2018-03-25 12:10:12 +0800

    Update helper

--- helper.php
+++ helper.php
@@ -5,3 +5,19 @@
  * Date: 2018/3/25
  * Time: 上午10:57
  */
+
+namespace PHPUtils;
+
+require dirname(__FILE__) . "/vendor/autoload.php";
+
+
+if (!function_exists('length')) {
+    function length(string $value, $encoding = null)
+    {
+        return Str::length($value, $encoding);
+    }
+}
+
+
+
+
