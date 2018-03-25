b20cb4d5acc2e661253caca382eba3a278636833
石金融 <aieruishi@gmail.com>
2018-03-25 12:10:49 +0800

    Update Debug

--- src/Debug.php
+++ src/Debug.php
@@ -101,35 +101,4 @@ class Debug
         return round($size, $dec) . " " . $a[$pos];
     }
 
-
-    /**
-     * 浏览器友好的变量输出
-     * @param mixed $var 变量
-     * @param boolean $echo 是否输出 默认为true 如果为false 则返回输出字符串
-     * @param string $label 标签 默认为空
-     * @param integer $flags htmlspecialchars flags
-     * @return void|string
-     */
-    public static function dump($var, $echo = true, $label = null, $flags = ENT_SUBSTITUTE)
-    {
-        $label = (null === $label) ? '' : rtrim($label) . ':';
-        ob_start();
-        var_dump($var);
-        $output = ob_get_clean();
-        $output = preg_replace('/\]\=\>\n(\s+)/m', '] => ', $output);
-        if (PHP_SAPI == 'cli') {
-            $output = PHP_EOL . $label . $output . PHP_EOL;
-        } else {
-            if (!extension_loaded('xdebug')) {
-                $output = htmlspecialchars($output, $flags);
-            }
-            $output = '<pre>' . $label . $output . '</pre>';
-        }
-        if ($echo) {
-            echo($output);
-            return;
-        } else {
-            return $output;
-        }
-    }
 }
