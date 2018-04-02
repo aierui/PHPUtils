0950c6f1fa3404ae69e0bba7df63110d6c88fa9b
石金融 <imland@outlook.com>
2018-04-02 23:01:20 +0800

    Add Http

---
+++ src/Http.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * User: aierui
+ * Email: aieruishi@gmail.com
+ * Date: 2018/4/1
+ * Time: 下午11:13
+ */
+
+namespace PHPUtils;
+
+
+class Http
+{
+
+
+}
---
+++ tests/ResponseTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * User: aierui
+ * Email: aieruishi@gmail.com
+ * Date: 2018/4/1
+ * Time: 下午11:48
+ */
+
+namespace Tests;
+
+
+class ResponseTest
+{
+
+}
