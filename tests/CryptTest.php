534575763720b9a93c8710ea91c2042df24355c9
aierui <aieruishi@gmail.com>
2018-05-13 23:37:20 +0800

    Add CryptTest File

--- tests/CryptTest.php
+++ tests/CryptTest.php
@@ -5,3 +5,24 @@
  * Date: 2018/3/31
  * Time: 下午6:29
  */
+
+namespace Tests;
+
+use PHPUtils\Crypt;
+use PHPUnit\Framework\TestCase;
+
+
+class CryptTest extends TestCase
+{
+
+    public function testRc4_encrypt()
+    {
+        echo 1;
+        $key = 'aierui';
+        $data= 'FooBar';
+        $code = Crypt::rc4_encrypt('Foobar', $key);
+        $this->assertSame(true, true);
+    }
+
+
+}
