ce9d17e6c81b3bbd832a7f49001083226eaa6351
aierui <aieruishi@gmail.com>
2018-05-13 23:36:39 +0800

    add crypt file

--- src/Crypt.php
+++ src/Crypt.php
@@ -8,19 +8,45 @@
 
 namespace PHPUtils;
 
-class Crypt{
+class Crypt
+{
 
-    public static function test()
-    {
+    public static function rc4_encrypt($data, $key) {
+        $s = array();
+        for ($i=0; $i<256; $i++) {
+            $s[$i] = $i;
+        }
 
+        $j = 0;
+        $key_len = strlen($key);
+        for ($i=0; $i<256; $i++) {
+            $j = ($j + $s[$i] + ord($key[$i % $key_len])) % 256;
+            //swap
+            $x = $s[$i];
+            $s[$i] = $s[$j];
+            $s[$j] = $x;
+        }
+        $i = 0;
+        $j = 0;
+        $ct = '';
+        $data_len = strlen($data);
+        for ($y=0; $y< $data_len; $y++) {
+            $i = ($i + 1) % 256;
+            $j = ($j + $s[$i]) % 256;
+            //swap
+            $x = $s[$i];
+            $s[$i] = $s[$j];
+            $s[$j] = $x;
+            $ct .= $data[$y] ^ chr($s[($s[$i] + $s[$j]) % 256]);
+        }
+        return $ct;
     }
 
-	public static function encrypt(){
 
-	}
+    public static function rc4_decrypt($data, $key) {
+        return self::rc4_encrypt($data, $key);
+    }
 
-	public static function decrypt()
-	{
-	}
-	
+
+
 }
