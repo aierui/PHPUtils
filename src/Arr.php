4a25bce1f8973d7657536df7f7ab59307cf5ed09
aieruishi <aieruishi@gmail.com>
2018-08-10 14:39:25 +0800

    update array

--- src/Arr.php
+++ src/Arr.php
@@ -104,42 +104,6 @@ class Arr
     }
 
 
-    /**
-     * 合并多个数组
-     * @param array[] ...$arrays
-     * @return array
-     */
-    public static function merge(array ... $arrays): array
-    {
-        $result = [];
-        $total = func_num_args();
-        for ($i = 0; $i < $total; $i++) {
-            $arr = func_get_arg($i);
-            $assoc = self::isAssoc($arr);
-            foreach ($arr as $key => $val) {
-                if (isset($result[$key])) {
-                    if (is_array($val) && is_array($result[$key])) {
-                        if (self::isAssoc($val)) {
-                            $result[$key] = self::merge($result[$key], $val);
-                        } else {
-                            $diff = array_diff($val, $result[$key]);
-                            $result[$key] = array_merge($result[$key], $diff);
-                        }
-                    } else {
-                        if ($assoc) {
-                            $result[$key] = $val;
-                        } elseif (!in_array($val, $result, true)) {
-                            $result[] = $val;
-                        }
-                    }
-                } else {
-                    $result[$key] = $val;
-                }
-            }
-        }
-        return $result;
-    }
-
 
     /**
      * 递归的数组合并实现
@@ -306,33 +270,6 @@ class Arr
     }
 
 
-    /**
-     * @param array $array
-     * @param $key_field
-     * @param null $value_field
-     * @param bool $force_string_key
-     * @return array
-     */
-    public static function hashMap(array $array, $key_field, $value_field = null, $force_string_key = false): array
-    {
-        if (!$array) {
-            return [];
-        }
-        $result = [];
-        if ($value_field) {
-            foreach ($array as $value) {
-                $key = $force_string_key ? (string)$value[$key_field] : $value[$key_field];
-                $result[$key] = $value[$value_field];
-            }
-        } else {
-            foreach ($array as $value) {
-                $key = $force_string_key ? (string)$value[$key_field] : $value[$key_field];
-                $result[$key] = $value;
-            }
-        }
-        return $result;
-    }
-
 
     /**
      * 将二维数组按照指定字段的值分组
@@ -346,70 +283,6 @@ class Arr
         foreach ($array as $value) {
             $key = $value[$key_field];
             $result[$key][] = $value;
-        }
-        return $result;
-    }
-
-
-    /**
-     * 将平面的二维数组按照指定的字段转换为树状结构
-     * @param array $array
-     * @param $key_node_id 节点ID字段名
-     * @param string $key_parent_id 节点父ID字段名
-     * @param string $key_childrens 保存子节点的字段名
-     * @param null $refs 是否在返回结果中包含节点引用
-     * @return array
-     */
-    public static function tree(array $array, $key_node_id, $key_parent_id = 'parent_id', $key_childrens = 'childrens', & $refs = null): array
-    {
-        $refs = [];
-        foreach ($array as $offset => $row) {
-            $array[$offset][$key_childrens] = array();
-            $refs[$row[$key_node_id]] = &$array[$offset];
-        }
-
-        $tree = [];
-        foreach ($array as $offset => $row) {
-            $parent_id = $row[$key_parent_id];
-            if ($parent_id) {
-                if (!isset($refs[$parent_id])) {
-                    $tree[] = &$array[$offset];
-                    continue;
-                }
-                $parent = &$refs[$parent_id];
-                $parent[$key_childrens][] = &$array[$offset];
-            } else {
-                $tree[] = &$array[$offset];
-            }
-        }
-
-        return $tree;
-    }
-
-    /**
-     * 将树形数组展开为平面的数组
-     *
-     * 这个方法是 tree() 方法的逆向操作。
-     *
-     * @param array $tree 树形数组
-     * @param string $key_childrens 包含子节点的键名
-     *
-     * @return array 展开后的数组
-     */
-
-    public static function treeToArray(array $tree, $key_childrens = 'childrens'): array
-    {
-        $result = [];
-        if (isset($tree[$key_childrens]) && is_array($tree[$key_childrens])) {
-            $childrens = $tree[$key_childrens];
-            unset($tree[$key_childrens]);
-            $result[] = $tree;
-            foreach ($childrens as $node) {
-                $result = array_merge($result, self::treeToArray($node, $key_childrens));
-            }
-        } else {
-            unset($tree[$key_childrens]);
-            $result[] = $tree;
         }
         return $result;
     }
